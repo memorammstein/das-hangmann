6d3638715e09eb1aba83a58cffbdab6022b6e60e
memorammstein <engel_live_aus_rammsteinland@hotmail.com>
2019-05-15 17:37:48 -0500

    Init new game with random word

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,12 @@
 import types from '../action-types';
+import getRandomWords from 'random-words';
 
-export function initGame({ difficulty }) {
+export function initGame({ level }) {
   const payload = {
+    word: '',
     mistakes: [],
     hits: []
   };
+  payload.word = getRandomWords({ exactly: level || 1, join: ' ' });
   return { type: types.INIT_GAME, payload };
 }
