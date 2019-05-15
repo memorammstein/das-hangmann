4d5a5244e0047bab7a2f357edb7f601373fcbbbc
memorammstein <engel_live_aus_rammsteinland@hotmail.com>
2019-05-15 15:30:34 -0500

    Remove reference to hangman

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<App hangman={Hangman} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
