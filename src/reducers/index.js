dc3ad642f20f82aef6c26aa8b0cc6fe025dcea5e
memorammstein <engel_live_aus_rammsteinland@hotmail.com>
2019-05-15 18:31:00 -0500

    Add guess letter reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -13,9 +13,41 @@ function rootReducer(state = initialState, action) {
   switch (action.type) {
     case types.INIT_GAME:
       return Object.assign({}, state, action.payload);
+    case types.GUESS_LETTER:
+      return Object.assign({}, state, guessLetter(state, action.payload.letter));
     default:
       return state;
   }
 };
 
 export default rootReducer;
+
+function guessLetter(state, letter) {
+  const partialState = {};
+  if (state.word.includes(letter)) {
+    partialState.hits = state.hits.concat(letter);
+    let hasWon = true;
+    const wordSet = new Set(state.word);
+    for (const letter of wordSet) {
+      if (letter.match(/[a-z]/i) && state.hits.includes(letter) === false) {
+        hasWon = false;
+      }
+    }
+    if (hasWon) {
+      partialState.in_progress = false;
+    }
+  } else {
+    partialState.mistakes = state.mistakes.concat(letter);
+    if (partialState.mistakes.length === state.opportunities) {
+      partialState.in_progress = false;
+    }
+  }
+  if (partialState.in_progress === false) {
+    partialState.history = state.history.concat({
+      word: state.word,
+      mistakes: state.mistakes,
+      hits: state.hits
+    });
+  }
+  return partialState;
+}
