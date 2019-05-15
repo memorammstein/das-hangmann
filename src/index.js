c29c528b77dda72038f4b0c7c666e1f3f1a748ef
memorammstein <engel_live_aus_rammsteinland@hotmail.com>
2019-05-15 15:07:51 -0500

    Crate components directory

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Hangman from './hangman';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(<App hangman={Hangman} />, document.getElementById('root'));
