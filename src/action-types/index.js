4ad415393a43feb028830022d6a81c093d0510bf
memorammstein <engel_live_aus_rammsteinland@hotmail.com>
2019-05-15 16:44:31 -0500

    Describe initial state. Create action types structure

---
+++ src/action-types/index.js
@@ -0,0 +1,4 @@
+export const types = {
+  INIT_GAME: 'INIT_GAME',
+  GUESS_LETTER: 'GUESS_LETTER',
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,18 @@
+import types from '../action-types';
+
 const initialState = {
+  in_progress: false,
+  word: '',
+  opportunities: 0,
+  mistakes: [],
+  hits: [],
+  history: []
 };
 
 function rootReducer(state = initialState, action) {
   switch (action.type) {
+    case types.INIT_GAME:
+      break;
     default:
       return state;
   }
