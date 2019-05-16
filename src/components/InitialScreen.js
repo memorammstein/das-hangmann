691826bf3988b203a9ae1509643a95d8f236cd52
memorammstein <engel_live_aus_rammsteinland@hotmail.com>
2019-05-15 23:20:46 -0500

    Connect Initial Screen to store

--- src/components/InitialScreen.js
+++ src/components/InitialScreen.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { initGame } from '../actions';
 
-const InitialScreen = () => {
+const mapDispatchToProps = dispatch => ({
+  initGame: options => { dispatch(initGame(options || {})) }
+});
+
+const ConnectedInitialScreen = ({ initGame }) => {
+  const handleStartButtonClick = event => {
+    initGame();
+  };
+
   return (
     <div className="initialScreen">
       <div className="cover">
@@ -8,11 +18,13 @@ const InitialScreen = () => {
         <h2>the best game I was able to make in a day</h2>
       </div>
       <div>
-        <button>START</button>
+        <button onClick={handleStartButtonClick}>START</button>
         <button>HISTORY</button>
       </div>
     </div>
   );
 };
+
+const InitialScreen = connect(null, mapDispatchToProps)(ConnectedInitialScreen);
 
 export default InitialScreen;
