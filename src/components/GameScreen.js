027b11377865606ce05c09607055d9d5688eaa01
memorammstein <engel_live_aus_rammsteinland@hotmail.com>
2019-05-16 01:26:49 -0500

    Style Game Screen

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -3,10 +3,11 @@ import Hangman from './Hangman';
 import Word from './Word';
 import Mistakes from './Mistakes';
 import LetterInput from './LetterInput';
+import './GameScreen.css';
 
 const GameScreen = () => {
   return(
-    <div className="gameScreen">
+    <div className="game-screen">
       <Hangman />
       <Word />
       <Mistakes />
