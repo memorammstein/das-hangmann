ab6bc0d825fd9c7fb8e06cedc99b8deea453e7df
memorammstein <engel_live_aus_rammsteinland@hotmail.com>
2019-05-15 20:40:19 -0500

    Render hangman notice. Connect it to store

--- src/components/Hangman.js
+++ src/components/Hangman.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const Hangman = () => {};
+const mapStateToProps = state => ({
+  numberOfMistakes: state.mistakes.length,
+  opportunities: state.opportunities
+});
+
+const ConnectedHangman = ({ numberOfMistakes, opportunities }) => {
+  const notDeadView = (
+    <div>
+      <span>chances left for you: <strong>{opportunities - numberOfMistakes}</strong></span>
+      <br />
+      <span>get it right or start saying bye-bye</span>
+    </div>
+  );
+  const deadView = (
+    <div>
+      <span>You're dead my friend</span>
+      <br />
+      <span>¯\_(ツ)_/¯</span>
+    </div>
+  );
+  return numberOfMistakes === opportunities ? deadView : notDeadView;
+};
+
+const Hangman = connect(mapStateToProps)(ConnectedHangman);
 
 export default Hangman;
