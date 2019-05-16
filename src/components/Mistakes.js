a113f783a498add2586f0dbd4fd4d6dc5813a120
memorammstein <engel_live_aus_rammsteinland@hotmail.com>
2019-05-15 21:38:42 -0500

    Render Mistakes. Style component. Connect to store

--- src/components/Mistakes.js
+++ src/components/Mistakes.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import './Mistakes.css';
 
-const Mistakes = () => {};
+const mapStateToProps = state => ({
+  mistakes: state.mistakes
+});
+
+const ConnectedMistakes = ({ mistakes }) => {
+  const letters = mistakes.map((letter, index) => {
+    return <span key={index}>{letter}</span>;
+  })
+  return (
+    <div className="mistakes-container">
+      <div>Mistakes you've made:</div>
+      <div className="mistakes">{letters}</div>
+    </div>
+  );
+};
+
+const Mistakes = connect(mapStateToProps)(ConnectedMistakes);
 
 export default Mistakes;
