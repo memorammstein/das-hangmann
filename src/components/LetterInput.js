f36265317d437e32ff09fb18a71e7a323d433917
memorammstein <engel_live_aus_rammsteinland@hotmail.com>
2019-05-15 22:34:39 -0500

    Render letter input component

--- src/components/LetterInput.js
+++ src/components/LetterInput.js
@@ -1,5 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const LetterInput = () => {};
+const validOptions = 'abcdefghijklmnopqrstuvwxyz';
+
+const LetterInput = () => {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleSelectChange = event => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  const options = validOptions.split('').map((option, index) => (
+    <option value={option} key={index}>{option}</option>
+  ));
+  const selectLetter = (
+    <select value={selectedOption} onChange={handleSelectChange}>
+      <option value="" disabled>select something</option>
+      {options}
+    </select>
+  );
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Select your letter:
+          {selectLetter}
+        </label>
+        <input type="submit" value="let's try it"/>
+      </form>
+    </div>
+  );
+};
 
 export default LetterInput;
